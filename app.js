070bcfe5cfe4f6add224929a12290d0df490c7ff
SilverFishCat <SilverFishCat@users.noreply.github.com>
2018-04-30 10:43:16 +0300

    Check only the first character in a messge for prefix

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ client.on("message", async message => {
 
   if(message.author.bot) return;
 
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if(message.content[0] !== config.prefix) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();

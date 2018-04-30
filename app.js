88b2861ca780f39675d77f2ec46411dc73bfcc28
SilverFishCat <SilverFishCat@users.noreply.github.com>
2018-04-30 11:00:18 +0300

    Removed activity setting onGuildCreate/Delete

--- app.js
+++ app.js
@@ -30,19 +30,16 @@ function callWarframeAPI(noun) {
 
 
 client.on("ready", () => {
-  
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   client.user.setActivity(`${config.prefix}help | In Dev`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`${config.prefix}help | In Dev`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`${config.prefix}help | In Dev`);
 });
 
 client.on("message", async message => {
